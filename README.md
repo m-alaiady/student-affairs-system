9cb11690e4dfe79fe1afcedced80a7bf624ba0b0
unknown <alaiady@outlook.sa>
2022-03-27 21:17:13 +0300

    removing

--- README.md
+++
@@ -1,6 +0,0 @@
-# Arab-Open-University-Project
-
-1. 2 Some modification from m-alaiady
-
-2. Zaiyad
-
--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $con=mysqli_connect('localhost','root','','sis project');
-
-    if(!$con)
-    {
-        die(' Please Check Your Connection'.mysqli_error($con));
-    }
-?>
