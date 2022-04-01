a9771b2d90650fa1bbc8c16cc5b6ff7a14f0000f
zaiyad1 <zyadxx2009@hotmail.com>
2022-04-01 19:51:20 +0300

    editing

--- Template/t1.php
+++ Template/t1.php
@@ -19,7 +19,10 @@ $data = mysqli_fetch_assoc($result);
 
      <!-- Start of the logo coding -->
 <head>
-    
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
 
      <div class="logo">
       
--- home.php
+++ home.php
@@ -8,11 +8,8 @@ include'template/t1.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <title>SIS | Home</title>
+    
+    <title>SIS | Home</title> 
 
 
 
