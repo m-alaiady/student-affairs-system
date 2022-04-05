04c4f071d4e99c2b74f3c2b48b63f05982805b8e
zaiyad1 <zyadxx2009@hotmail.com>
2022-04-05 05:22:15 +0300

    uploading

--- Template/t1.php
+++ Template/t1.php
@@ -176,7 +176,6 @@ $path = "http://" . $_SERVER['HTTP_HOST'] . "/" . $proj_name;
         <li><a href="#">Exam schedule</a></li>
         <li><a href="#">Exam certificate</a></li>
         <li><a href="#">Exam postpone</a></li>
-            <li><a href="#">Upload exam excuse</a></li>
             <li><a href="#">Exam objection</a></li>
             <li><a href="#">Register for makeup exam</a></li>
             <li><a href="#">Exam-different branch</a></li>
