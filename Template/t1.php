0fb3a0a5e29dc9319841826ce50d76dbf7a95ff1
mohammed <alaiady@outlook.sa>
2022-04-25 06:07:24 +0300

    adding pay button

--- Template/t1.php
+++ Template/t1.php
@@ -2,7 +2,9 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 
 if(!isset($_SESSION['student_id']))
 {
--- registration/register.php
+++ registration/register.php
@@ -4,39 +4,58 @@
 
 session_start();
 
-// print_r($_SESSION); die;
-
-
-if(isset($_SESSION['student_id']))
-{
-    if(isset($_SESSION['paid'])){
-        header("location:courses.php?page=1");
+if (isset($_SESSION['student_id'])) {
+    if (isset($_SESSION['paid'])) {
+        header("location:courses.php");
+        exit;
     }
-}
-else
-{
+} else {
     header("location:../index.php");
 }
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+// it is to include the SIS project project
+require_once("../connection.php");
+
+// this will include the design of the page such as: background, head, footer, and side menu
+include("../template/t1.php");
 
 ?>
 
 <html>
-<head>
 
+<head>
+    <style>
+        .pay-btn-section{
+            position: absolute;
+            margin-top:10em;
+            margin-left: 30em;
+        }
+        .pay-btn-section input[type='submit']{
+            padding: 1em 2em;
+            font-size: 1.5em;
+        }
+    </style>
 </head>
+
 <body>
 
-<form method="post">
-    <input type="hidden" name="paid" value="1" />
-    <input type="submit" name="pay" value="Pay" />
-</form>
-<?php
+    <div class="pay-btn-section">
+        <form method="post">
+            <input type="hidden" name="paid" value="1" />
+            <input type="submit" name="pay" value="Pay" />
+        </form>
+    </div>
+    <?php
 
-if(isset($_POST['pay'])){
-    $_SESSION['paid']=true;
-    header("Refresh:0");
-}
+    if (isset($_POST['pay'])) {
+        $_SESSION['paid'] = true;
+        echo "<script>window.location = 'courses.php'</script>";
+    }
 
-?>
+    ?>
 </body>
+
 </html>
